338d15dd8bf01c5efe812a358d65e5e72b22f1a7
Eduardo Dadalto <edadaltocg@gmail.com>
2024-06-06 14:55:47 +0200

    fix fetch url

--- src/js/waitlist.js
+++ src/js/waitlist.js
@@ -6,16 +6,13 @@ document
     const responseMessage = document.getElementById("response-message");
 
     try {
-      const response = await fetch(
-        "http://localhost/backend/api/collections/leads/records",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email: email }),
+      const response = await fetch("/backend/api/collections/leads/records", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-      );
+        body: JSON.stringify({ email: email }),
+      });
 
       if (response.ok) {
         responseMessage.textContent = "You have been added to the waitlist!";
